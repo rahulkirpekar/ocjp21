257b9df7acf8b3e5eece9c0512fe173d0efb765b
rahul kirpekar <rahul.kirpekar@gmail.com>
2025-01-27 14:38:26 +0530

    jdbc-connection

--- src/jdbctopic/util/DBConnection.java
+++ src/jdbctopic/util/DBConnection.java
@@ -1,29 +1,52 @@
 package jdbctopic.util;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.util.Properties;
 
 // Java Program---MySql
 public class DBConnection 
 {
-	// 1) Make Database credentials
-	private static final String URLNAME     = "jdbc:mysql://localhost:3306/ocjp21";
-	private static final String DRIVERCLASS = "com.mysql.cj.jdbc.Driver";
-	private static final String USERNAME    = "root";
-	private static final String PASSWORD	= "root";	
-
 	// 2) create getConnection method
 	public static Connection getConnection() 
 	{
+		String urlName = null;
+		String driverClass = null;
+		String userName = null;
+		String password = null;
+		try 
+		{
+			FileInputStream fin = new FileInputStream("C:\\Users\\Royal\\eclipse-workspace\\ocjp21\\dbconfig.properties");
+		
+			Properties prop = new Properties();
+			
+			prop.load(fin);
+		
+			urlName = prop.getProperty("URLNAME").trim();
+			driverClass = prop.getProperty("DRIVERCLASS").trim();
+			userName = prop.getProperty("USERNAME").trim();
+			password = prop.getProperty("PASSWORD").trim();
+			
+		} catch (FileNotFoundException e) 
+		{
+			e.printStackTrace();
+		} catch (IOException e) 
+		{
+			e.printStackTrace();
+		}
+		
 		Connection conn = null;
 		try 
 		{
 			// 3) Load Driver class
-			Class.forName(DRIVERCLASS);
+			Class.forName(driverClass);
 			
 
 			// 4) get Database connection by DriverManager's getConnection method
-			conn = DriverManager.getConnection(URLNAME, USERNAME, PASSWORD);
+			conn = DriverManager.getConnection(urlName, userName, password);
 			
 		} catch (Exception e) 
 		{
